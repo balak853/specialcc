e88fe5eed8590bf0b9fbed1c4219a4eeb7e6ee80
balak853 <kingcrp8@gmail.com>
2025-03-21 12:36:51 +0530

    response.py

--- BOT/AUTH/Braintree Auth [ b3 ]/response.py
+++ BOT/AUTH/Braintree Auth [ b3 ]/response.py
@@ -7,101 +7,79 @@ async def get_charge_resp(result, user_id, fullcc):
     try:
 
         if type(result) == str:
-            status   = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
+            status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
             response = result
-            hits     = "NO"
-
+            hits = "NO"
 
             if (
-                '"success":true,"data":"status":"succeeded"' in result
-                or 'succeeded' in result
+                "Nice! New payment method added" in result
             ):
-                    status = "𝐀𝐩𝐩𝐫𝐨𝐯𝐞𝐝 ✅"
-                    response = "Stripe Auth 🔥"
-                    hits = "YES"
+                status = "𝐀𝐩𝐩𝐫𝐨𝐯𝐞𝐝 ✅"
+                # response = "Approved ✅"
+                response = "1000: Approved"
+                hits = "YES"
+                await forward_resp(fullcc, "BRAINTREE AUTH", response)
 
-            elif (
-                '{"success":true,"data":{"status":"requires_action"' in result
-                or 'succeeded' in result
-            ):
-                    status = "𝐀𝐩𝐩𝐫𝐨𝐯𝐞𝐝 ✅"
-                    response = "1000: Approved"
-                    hits = "YES"
-                    
-
-
-
-
-
-
-
-
-
-            elif ("Your card was declined" in result):
-                    status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
-                    response = "Card was declined"
-                    hits = "NO"
+            elif ("Status code cvv: Gateway Rejected: cvv" in result):
+                status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
+                response = "Gateway Rejected: cvv"
+                hits = "NO"
 
             elif ("Declined - Call Issuer" in result):
-                    status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
-                    response = "Declined - Call Issuer"
-                    hits = "NO"
+                status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
+                response = "Declined - Call Issuer"
+                hits = "NO"
 
             elif ("Cannot Authorize at this time" in result):
-                    status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
-                    response = "Cannot Authorize at this time"
-                    hits = "NO"
+                status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
+                response = "Cannot Authorize at this time"
+                hits = "NO"
 
             elif ("Processor Declined - Fraud Suspected" in result):
-                    status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
-                    response = "Fraud Suspected"
-                    hits = "NO"
+                status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
+                response = "Fraud Suspected"
+                hits = "NO"
 
             elif "Status code risk_threshold: Gateway Rejected: risk_threshold" in result:
-                    status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
-                    response = "Gateway Rejected: risk_threshold"
-                    hits = "NO"
+                status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
+                response = "Gateway Rejected: risk_threshold"
+                hits = "NO"
 
             elif ("We're sorry, but the payment validation failed. Declined - Call Issuer" in result or
-                    "Payment failed: Declined - Call Issuer" in result
-                    ):
-                    status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
-                    response = "Declined - Call Issuer"
-                    hits = "NO"
-
-            elif "Payment Intent Creation Failed ❌" in result:
-                status   = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
-                response = "Payment Intent Creation Failed ❌"
-                hits     = "NO"
-                await refundcredit(user_id)
+                  "Payment failed: Declined - Call Issuer" in result
+                  ):
+                status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
+                response = "Declined - Call Issuer"
+                hits = "NO"
 
             elif "ProxyError" in result:
-                status   = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
-                response = "Proxy Connection Refused ❌"
-                hits     = "NO"
+                status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
+                response = "Proxy Connection Refused"
+                hits = "NO"
                 await refundcredit(user_id)
 
             else:
                 status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
-                response = await find_between(result , "System was not able to complete the payment. ", ".")
-                if response is None:
-                    response = "Card Declined"
-                    await result_logs(fullcc, "Stripe Charge", result)
-                response = response + " ❌"
+                try:
+                    response = result.split('"message": "')[
+                        1].split('"')[0] + " ❌"
+                except:
+                    response = result
+                    await result_logs(fullcc, "Braintree Auth", result)
                 hits = "NO"
 
-            json = {
-                "status": status,
-                "response": response,
-                "hits": hits,
-                "fullz": fullcc,
-            }
-            return json 
+        json = {
+            "status": status,
+            "response": response,
+            "hits": hits,
+            "fullz": fullcc,
+        }
+        return json
 
     except Exception as e:
-        status   = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
+        status = "𝐃𝐞𝐜𝐥𝐢𝐧𝐞𝐝 ❌"
         response = str(e) + " ❌"
-        hits     = "NO"
+        hits = "NO"
 
         json = {
             "status": status,
