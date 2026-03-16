0012bdf89fe1788303d9a9026968e089f24e9989
balak853 <kingcrp8@gmail.com>
2026-03-16 07:57:06 +0530

    Refactor MongoDB connection and database access

--- mongodb.py
+++ mongodb.py
@@ -1,20 +1,17 @@
+import traceback
 import pymongo
 
-client = pymongo.MongoClient(
-"mongodb+srv://balak:Balak12345@cluster0.fdc3y.mongodb.net/?retryWrites=true&w=majority"
-)
+client = pymongo.MongoClient("mongodb+srv://balak:Balak12345@cluster0.fdc3y.mongodb.net/?retryWrites=true&w=majority")
 
 try:
     client.admin.command("ping")
-    print("MONGODB CONNECTED SUCCESSFULLY ✅")
+    print("MONGODB CONNECTED SUCCESSFULLY")
 except Exception as e:
-    print("MONGODB CONNECTION FAILED ❌", e)
+    print("MONGODB CONNECTION FAILED", e)
 
 folder = client["BALAK_DATABASE"]
-
-usersdb = folder["USERSDB"]
-chats_auth = folder["CHATS_AUTH"]
-gcdb = folder["GCDB"]
-
-sksdb = client["SKS_DATABASE"]["SKS"]
-confdb = client["SKS_DATABASE"]["CONF_DATABASE"]
+usersdb = folder.USERSDB
+chats_auth = folder.CHATS_AUTH
+gcdb = folder.GCDB
+sksdb = client["SKS_DATABASE"].SKS
+confdb = client["SKS_DATABASE"].CONF_DATABASE
